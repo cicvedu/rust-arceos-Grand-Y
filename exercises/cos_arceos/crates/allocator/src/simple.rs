64a5c9e2ccc27d02b45303924c6f4ef055644c69
wdf <wdf>
2023-08-23 14:34:40 +0800

    1

--- exercises/cos_arceos/crates/allocator/src/simple.rs
+++ exercises/cos_arceos/crates/allocator/src/simple.rs
@@ -2,22 +2,30 @@
 //!
 //! TODO: more efficient
 
-use core::alloc::Layout;
+use core::{alloc::Layout, f32::consts::E};
 use core::num::NonZeroUsize;
 
 use crate::{AllocResult, BaseAllocator, ByteAllocator};
 
-pub struct SimpleByteAllocator;
+pub struct SimpleByteAllocator {
+    allocations: i32,
+    start: usize,
+    next: usize,
+    size: usize,
+}
 
 impl SimpleByteAllocator {
     pub const fn new() -> Self {
-        Self {}
+        Self { allocations: 0, start: 0, next: 0, size: 0 }
     }
 }
 
 impl BaseAllocator for SimpleByteAllocator {
     fn init(&mut self, _start: usize, _size: usize) {
-        todo!();
+        self.allocations = 0;
+        self.start = _start;
+        self.next = _start;
+        self.size = _size;
     }
 
     fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
@@ -27,22 +35,29 @@ impl BaseAllocator for SimpleByteAllocator {
 
 impl ByteAllocator for SimpleByteAllocator {
     fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-        todo!();
+        if _layout.size() > (self.size - (self.next - self.start)) {
+            Err(crate::AllocError::NoMemory)
+        } else {
+            let start_ptr = self.next;
+            self.next += _layout.size();
+            self.allocations += 1;
+            Ok(NonZeroUsize::new(start_ptr).unwrap())
+        }
     }
 
     fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-        todo!();
+        self.allocations -= 1;
     }
 
     fn total_bytes(&self) -> usize {
-        todo!();
+        self.size
     }
 
     fn used_bytes(&self) -> usize {
-        todo!();
+        self.next
     }
 
     fn available_bytes(&self) -> usize {
-        todo!();
+        self.size-(self.next-self.start)
     }
 }
